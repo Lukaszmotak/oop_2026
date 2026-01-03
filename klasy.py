dd2fe603fd3bc988fd69c4f718b0eb2f39acbe3f
Lukaszmotak <lukaszmotak19@gmail.com>
2026-01-03 11:08:24 +0100

    Initial version of the class Czlowiek

---
+++ klasy.py
@@ -0,0 +1,17 @@
+# Klasa = szablon, przepis
+class Czlowiek:
+    #Istota
+    gatunek = "Homo Sapiens"
+    def __init__(self):
+        #Konstruktor
+        #Akt istnienia
+        print("Niech powstanie Czlowiek")
+
+# Powstawanie obiektu
+# Gotowanie z przepisu
+
+adam = Czlowiek()
+print(adam.gatunek)
+
+
+
